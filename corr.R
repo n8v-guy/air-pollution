13dca7cee2bcc8c82f4accddcfe4d3525967a49d
Max Shashkov <mshashkov@mshashkov-osx.local>
2014-06-13 19:34:24 +0400

    Implementation for 'corr' function

--- corr.R
+++ corr.R
@@ -10,20 +10,35 @@ corr <- function(directory, threshold = 0) {
   ## nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
+  
+  ret <- vector()
+  pos <- 1
+  #observCount <- complete("specdata", 1:332)
+  thresholdArr = observCount["id"][observCount["nobs"] >= threshold]
+  for(csv in thresholdArr) {
+    file <- sprintf("./%s/%03d.csv", directory, csv)
+    table <- read.csv(file)
+    valid = !is.na(table["sulfate"]) & !is.na(table["nitrate"])
+    sulf = table["sulfate"][valid]
+    nitr = table["nitrate"][valid]
+    ret[pos] <- cor(sulf, nitr)
+    pos <- pos + 1
+  }
+  ret
 }
 
 cr <- corr("specdata", 150)
-head(cr)
-summary(cr)
+print(head(cr))
+print(summary(cr))
 
 cr <- corr("specdata", 400)
-head(cr)
-summary(cr)
+print(head(cr))
+print(summary(cr))
 
 cr <- corr("specdata", 5000)
-summary(cr)
-length(cr)
+print(summary(cr))
+print(length(cr))
 
 cr <- corr("specdata")
-summary(cr)
-length(cr)
+print(summary(cr))
+print(length(cr))
