e8b2fe4a55ea37770254ba7dd561b5735acd20c5
Max Shashkov <mshashkov@mshashkov-osx.local>
2014-06-13 16:45:11 +0400

    Pollutant mean calculation first draft (seems incorrect)

--- pollutantmean.R
+++ pollutantmean.R
@@ -11,4 +11,23 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
+  
+  resultVector <- vector(mode="numeric")
+  pos <- 1
+  for(i in id) {
+    file <- sprintf("./%s/%03d.csv", directory, i)
+    table <- read.csv(file)
+    tableColumn <- table[pollutant]
+    tableColumnValid <- !is.na(table["sulfate"]) & !is.na(table["nitrate"])
+    cleanTableColumn <- tableColumn[tableColumnValid];
+    meanValue <- mean(cleanTableColumn)
+    resultVector[pos] <- meanValue
+    pos <- pos + 1
+  }
+  resultMean <- mean(resultVector)
+  round(resultMean,3)
 }
+
+print(pollutantmean("specdata", "sulfate", 1:10))
+print(pollutantmean("specdata", "nitrate", 70:72))
+print(pollutantmean("specdata", "nitrate", 23))
